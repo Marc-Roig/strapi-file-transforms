80de07a60bf0037cf9ca081029e171007cb70134
Marc-Roig <marc12info@gmail.com>
2023-06-12 11:04:37 +0200

    remove transformations

--- config/database.js
+++ config/database.js
@@ -1,14 +1,25 @@
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
+module.exports = ({ env }) => {
+  const sqlite = {
+    client: "sqlite",
     connection: {
-      host: env('PGHOST', '127.0.0.1'),
-      port: env.int('PGPORT', 5432),
-      database: env('PGDATABASE', 'strapi'),
-      user: env('PGUSER', 'strapi'),
-      password: env('PGPASSWORD', 'password'),
+      filename: ".tmp/data.db",
+    },
+    useNullAsDefault: true,
+  };
+
+  const postgres = {
+    client: "postgres",
+    connection: {
+      host: env("PGHOST", "127.0.0.1"),
+      port: env.int("PGPORT", 5432),
+      database: env("PGDATABASE", "strapi"),
+      user: env("PGUSER", "strapi"),
+      password: env("PGPASSWORD", "password"),
       ssl: env.bool(true),
     },
-    pool: { min: 0 }
-  },
-});
+  };
+
+  return {
+    connection: process.env.NODE_ENV === "development" ? sqlite : postgres,
+  };
+};
